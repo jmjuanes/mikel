e84cd36c9e24faa24c7bf8b3982416b05fc98a84
Josemi <josemijuanes@gmail.com>
2023-12-18 20:58:25 +0100

    Add tests for classMap and styleMap

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-import {html, render} from "./mikel.js";
+import {html, render, classMap, styleMap} from "./mikel.js";
 
 describe("html", () => {
     it("should render a basic element", () => {
@@ -39,5 +39,45 @@ describe("html", () => {
         el.querySelector(`div#test2`).click();
         expect(handleClick1).not.toHaveBeenCalled();
         expect(handleClick2).toHaveBeenCalled();
+    });
+});
+
+describe("classMap", () => {
+    it("should generate a valid class string", () => {
+        const className = classMap({
+            "class1": true,
+            "class2": false,
+            "class3": true,
+        });
+
+        expect(className).toEqual("class1 class3");
+    });
+
+    it("should return an empty string if no class object is provided", () => {
+        expect(classMap()).toEqual("");
+    });
+});
+
+describe("styleMap", () => {
+    it("should generate a valid style string", () => {
+        const styleName = styleMap({
+            "background-color": "red",
+            "margin": "5px 0px",
+        });
+
+        expect(styleName).toEqual("background-color:red;margin:5px 0px;");
+    });
+
+    it("should accept style attributes in camelCase format", () => {
+        const styleName = styleMap({
+            backgroundColor: "red",
+            paddingTop: "1rem",
+        });
+
+        expect(styleName).toEqual("background-color:red;padding-top:1rem;");
+    });
+
+    it("should return an empty string if no style object is provided", () => {
+        expect(styleMap()).toEqual("");
     });
 });
