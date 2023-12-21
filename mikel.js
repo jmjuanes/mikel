205066ec1dfec4991a2136e20e56cc4911a49217
Josemi Juanes <5751201+jmjuanes@users.noreply.github.com>
2023-12-21 16:40:14 +0100

    feat: enable dom diffing in render (#5)

--- mikel.js
+++ mikel.js
@@ -3,6 +3,9 @@ const NODE_TEXT_TYPE = Symbol.for("node.text");
 const NODE_COMMENT_TYPE = Symbol.for("node.comment");
 const NODE_HTML_TYPE = Symbol.for("node.html");
 
+// Private list with all available events
+const eventsNames = Object.keys(window).filter(n => n && n.startsWith("on"));
+
 // Convert an string in camelCase format to kebab-case
 const camelToKebabCase = str => str.replace(/[A-Z]/g, letter => `-${letter.toLowerCase()}`);
 
@@ -22,6 +25,20 @@ const getNodeType = node => {
         return node.tagName.toLowerCase();
     }
     return null;
+};
+
+// Gets node attributes
+const getNodeAttributes = node => {
+    return Object.fromEntries(Array.from(node?.attributes || []).map(item => {
+        return [item.name, item.value];
+    }));
+};
+
+const getNodeListeners = node => {
+    const eventsListeners = eventsNames
+        .filter(e => typeof node[e] === "function")
+        .map(eventName => ([eventName, node[eventName]]));
+    return Object.fromEntries(eventsListeners);
 };
 
 // Render html string
@@ -54,7 +71,7 @@ const registerElementEvents = (element, callback) => {
             const name = attrs.item(i).name;
             if (name.startsWith("on")) {
                 element.removeAttribute(name);
-                callback(element, name.slice(2).toLowerCase());
+                callback(element, name.toLowerCase());
             }
         }
     }
@@ -128,6 +145,73 @@ const compile = template => {
     return [result, events];
 };
 
+export const diff = (source, target) => {
+    const sourceNodes = Array.from(source.childNodes);
+    const targetNodes = Array.from(target.childNodes);
+    // 1. Check if source is empty
+    if (sourceNodes.length === 0) {
+        return targetNodes.forEach(node => {
+            source.appendChild(node);
+        });
+    }
+    const parent = sourceNodes[0].parentNode;
+    // 2. Check if source has more dom nodes
+    if (sourceNodes.length > targetNodes.length) {
+        const count = sourceNodes.length - targetNodes.length;
+        for (let i = count; i > 0; i--) {
+            parent.removeChild(sourceNodes[sourceNodes.length - i]);
+        }
+    }
+    // 3. Iterate over all child nodes of target
+    targetNodes.forEach((targetChild, index) => {
+        const sourceChild = sourceNodes[index];
+        // 3.1 Check if source child is not defined
+        if (!sourceChild) {
+            return parent.appendChild(targetChild);
+        }
+        // 3.2 Check if nodes are differente
+        if (getNodeType(sourceChild) !== getNodeType(targetChild)) {
+            return parent.replaceChild(targetChild, sourceChild);
+        }
+        // 3.3 Check if they are text nodes
+        if (getNodeType(targetChild) === NODE_TEXT_TYPE) {
+            sourceChild.textContent = targetChild.textContent;
+            return;
+        }
+        // 3.4 Patch attributes
+        const sourceAttributes = getNodeAttributes(sourceChild);
+        const targetAttributes = getNodeAttributes(targetChild);
+        Object.keys(targetAttributes).forEach(key => {
+            // If the attribute is not present in sourcedom then add it
+            // If exists but have different value, update the attribute
+            if (sourceAttributes[key] !== targetAttributes[key]) {
+                sourceChild.setAttribute(key, targetAttributes[key]);
+            }
+        });
+        // Remove attributes in source that are not in target
+        Object.keys(sourceAttributes).forEach(key => {
+            if (typeof targetAttributes[key] === "undefined") {
+                sourceChild.removeAttribute(key);
+            }
+        });
+        // 3.6 Patch events listeners
+        const sourceListeners = getNodeListeners(sourceChild);
+        const targetListeners = getNodeListeners(targetChild);
+        Object.keys(targetListeners).forEach(key => {
+            sourceChild[key] = targetListeners[key];
+        });
+        Object.keys(sourceListeners).forEach(key => {
+            if (typeof targetListeners[key] !== "function") {
+                delete sourceChild[key];
+            }
+        });
+        // 3.7 Check if target has child nodes
+        if (targetChild.hasChildNodes()) {
+            diff(sourceChild, targetChild);
+        }
+    });
+};
+
 // Returns a HTML template object
 export const html = (literal, ...values) => {
     return {
@@ -144,23 +228,10 @@ export const render = (parent, template) => {
     registerElementEvents(element, (child, eventName) => {
         const eventListener = events.shift();
         if (typeof eventListener === "function") {
-            child.addEventListener(eventName, eventListener);
+            child[eventName] = eventListener;
         }
     });
-    // Check for clearing parent
-    if (parent.hasChildNodes()) {
-        parent.replaceChildren();
-    }
-    // Append child nodes
-    Array.from(element.childNodes).forEach(child => {
-        parent.appendChild(child);
-    });
-};
-
-// Mount a template object
-export const mount = (parent, template) => {
-    parent.replaceChildren();
-    parent.appendChild(render(template));
+    diff(parent, element);
 };
 
 // Classmap helper
@@ -181,7 +252,6 @@ export const styleMap = styleList => {
 export default {
     html,
     render,
-    mount,
     classMap,
     styleMap,
 };
--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-import {html, render, classMap, styleMap} from "./mikel.js";
+import {html, render, diff, classMap, styleMap} from "./mikel.js";
 
 describe("html", () => {
     let parent = null;
@@ -103,6 +103,46 @@ describe("html", () => {
             <div>World</div>
         `);
         expect(Array.from(parent.childNodes)).toHaveLength(2);
+    });
+});
+
+describe("diff", () => {
+    let source = null, target = null;
+    beforeEach(() => {
+        source = document.createElement("div");
+        target = document.createElement("div");
+    });
+
+    it("should add all nodes from target if source is empty", () => {
+        render(target, html`
+            <div id="el1"></div>
+            <div id="el2"></div>
+        `);
+        expect(Array.from(source.childNodes)).toHaveLength(0);
+        diff(source, target);
+        expect(Array.from(source.childNodes)).toHaveLength(2);
+        expect(source.querySelector("div#el1")).not.toBeNull();
+        expect(source.querySelector("div#el2")).not.toBeNull();
+    });
+
+    it("should remove extra dom nodes in source", () => {
+        render(source, html`<div></div><div></div>`);
+        expect(Array.from(source.childNodes)).toHaveLength(2);
+        diff(source, target);
+        expect(Array.from(source.childNodes)).toHaveLength(0);
+    });
+
+    it("should patch attributes in source", () => {
+        const handleClick = jest.fn();
+        render(source, html`<div></div>`);
+        render(target, html`
+            <div data-id="test" class="test" onClick="${handleClick}" style="color:red;"></div>
+        `);
+        diff(source, target);
+        expect(source.firstChild.dataset.id).toEqual("test");
+        expect(Array.from(source.firstChild.classList)).toContain("test");
+        expect(source.firstChild.onclick).toEqual(handleClick);
+        expect(source.firstChild.getAttribute("style")).toEqual("color:red;");
     });
 });
 
