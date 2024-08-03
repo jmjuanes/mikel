881692ec1d7135db817383b87a928c2e602d3b78
Josemi Juanes <5751201+jmjuanes@users.noreply.github.com>
2024-08-03 17:23:53 +0000

    Fix

--- test.js
+++ test.js
@@ -381,9 +381,12 @@ describe("utils", () => {
             const json = yaml([
                 `link1: "https://www.example1.com"`,
                 `link2: "https://www.example2.com"`,
+                `items:`,
+                `  - "https://www.example3.com"`,
             ]);
             assert.equal(json.link1, "https://www.example1.com");
             assert.equal(json.link2, "https://www.example2.com");
+            assert.equal(json.items[0], "https://www.example3.com");
         });
     });
 
