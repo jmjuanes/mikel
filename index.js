c4f7c17770c89720c3bc64bb366adb5f4b9425dd
Josemi Juanes <5751201+jmjuanes@users.noreply.github.com>
2024-09-07 11:31:34 +0200

    Move compiler method inside create

--- index.js
+++ index.js
@@ -99,94 +99,94 @@ const create = (template = "", options = {}) => {
     });
     const partials = options?.partials || {};
     const functions = options?.functions || {};
-
-
-
-    const compileTemplate = (data, output = []) => {
-        return null;
-    };
-    return compileTemplate;
-};
-
-const compile = (tokens, output, context, partials, helpers, vars, fn = {}, index = 0, section = "") => {
-    let i = index;
-    while (i < tokens.length) {
-        if (i % 2 === 0) {
-            output.push(tokens[i]);
-        }
-        else if (tokens[i].startsWith("@")) {
-            output.push(get(vars, tokens[i].slice(1).trim() ?? "_") ?? "");
-        }
-        else if (tokens[i].startsWith("!")) {
-            output.push(get(context, tokens[i].slice(1).trim()));
-        }
-        else if (tokens[i].startsWith("#") && typeof helpers[tokens[i].slice(1).trim().split(" ")[0]] === "function") {
-            const [t, ...args] = tokens[i].slice(1).trim().match(/(?:[^\s"]+|"[^"]*")+/g);
-            const j = i + 1;
-            output.push(helpers[t](...args.map(v => parse(v, context, vars)), {
-                context: context,
-                fn: (blockContext = {}, blockVars = {}, blockOutput = []) => {
-                    i = compile(tokens, blockOutput, blockContext, partials, helpers, {...vars, ...blockVars, root: vars.root}, fn, j, t);
-                    return blockOutput.join("");
-                },
-            }));
-            // Make sure that this block is executed at least once
-            if (i + 1 === j) {
-                i = compile(tokens, [], {}, {}, helpers, {}, {}, j, t);
+    // internal method to compile the template
+    const compile = (tokens, output, context, vars, index = 0, section = "") => {
+        let i = index;
+        while (i < tokens.length) {
+            if (i % 2 === 0) {
+                output.push(tokens[i]);
             }
-        }
-        else if (tokens[i].startsWith("#") || tokens[i].startsWith("^")) {
-            const t = tokens[i].slice(1).trim();
-            const value = get(context, t);
-            const negate = tokens[i].startsWith("^");
-            if (!negate && value && Array.isArray(value)) {
+            else if (tokens[i].startsWith("@")) {
+                output.push(get(vars, tokens[i].slice(1).trim() ?? "_") ?? "");
+            }
+            else if (tokens[i].startsWith("!")) {
+                output.push(get(context, tokens[i].slice(1).trim()));
+            }
+            else if (tokens[i].startsWith("#") && typeof helpers[tokens[i].slice(1).trim().split(" ")[0]] === "function") {
+                const [t, ...args] = tokens[i].slice(1).trim().match(/(?:[^\s"]+|"[^"]*")+/g);
                 const j = i + 1;
-                (value.length > 0 ? value : [""]).forEach(item => {
-                    i = compile(tokens, value.length > 0 ? output : [], item, partials, helpers, vars, fn, j, t);
-                });
+                output.push(helpers[t](...args.map(v => parse(v, context, vars)), {
+                    context: context,
+                    fn: (blockContext = {}, blockVars = {}, blockOutput = []) => {
+                        i = compile(tokens, blockOutput, blockContext, {...vars, ...blockVars, root: vars.root}, j, t);
+                        return blockOutput.join("");
+                    },
+                }));
+                // Make sure that this block is executed at least once
+                if (i + 1 === j) {
+                    i = compile(tokens, [], {}, {}, helpers, {}, {}, j, t);
+                }
+            }
+            else if (tokens[i].startsWith("#") || tokens[i].startsWith("^")) {
+                const t = tokens[i].slice(1).trim();
+                const value = get(context, t);
+                const negate = tokens[i].startsWith("^");
+                if (!negate && value && Array.isArray(value)) {
+                    const j = i + 1;
+                    (value.length > 0 ? value : [""]).forEach(item => {
+                        i = compile(tokens, value.length > 0 ? output : [], item, vars, j, t);
+                    });
+                }
+                else {
+                    const includeOutput = (!negate && !!value) || (negate && !!!value);
+                    i = compile(tokens, includeOutput ? output : [], context, vars, i + 1, t);
+                }
+            }
+            else if (tokens[i].startsWith(">")) {
+                const [t, v] = tokens[i].slice(1).trim().split(" ");
+                if (typeof partials[t] === "string") {
+                    compile(partials[t].split(tags), output, v ? get(context, v) : context, vars, 0, "");
+                }
+            }
+            else if (tokens[i].startsWith("=")) {
+                const [t, ...args] = tokens[i].slice(1).trim().match(/(?:[^\s"]+|"[^"]*")+/g);
+                if (typeof functions[t] === "function") {
+                    output.push(functions[t](...args.map(v => parse(v, context, vars))) || "");
+                }
+            }
+            else if (tokens[i].startsWith("/")) {
+                if (tokens[i].slice(1).trim() !== section) {
+                    throw new Error(`Unmatched section end: {{${tokens[i]}}}`);
+                }
+                break;
             }
             else {
-                const includeOutput = (!negate && !!value) || (negate && !!!value);
-                i = compile(tokens, includeOutput ? output : [], context, partials, helpers, vars, fn, i + 1, t);
+                output.push(escape(get(context, tokens[i].trim())));
             }
+            i = i + 1;
         }
-        else if (tokens[i].startsWith(">")) {
-            const [t, v] = tokens[i].slice(1).trim().split(" ");
-            if (typeof partials[t] === "string") {
-                compile(partials[t].split(tags), output, v ? get(context, v) : context, partials, helpers, vars, fn, 0, "");
-            }
-        }
-        else if (tokens[i].startsWith("=")) {
-            const [t, ...args] = tokens[i].slice(1).trim().match(/(?:[^\s"]+|"[^"]*")+/g);
-            if (typeof fn[t] === "function") {
-                output.push(fn[t](...args.map(v => parse(v, context, vars))) || "");
-            }
-        }
-        else if (tokens[i].startsWith("/")) {
-            if (tokens[i].slice(1).trim() !== section) {
-                throw new Error(`Unmatched section end: {{${tokens[i]}}}`);
-            }
-            break;
-        }
-        else {
-            output.push(escape(get(context, tokens[i].trim())));
-        }
-        i = i + 1;
-    }
-    return i;
+        return i;
+    };
+    // entry method to compile the template with the provided data object
+    const compileTemplate = (data = {}, output = []) => {
+        compile(template.split(tags), output, data, {}, 0, "");
+        return output.join("");
+    };
+    // assign api methods and return method to compile the template
+    return Object.assign(compileTemplate, {
+        registerHelper: (name, fn) => helpers[name] = fn,
+        registerFunction: (name, fn) => functions[name] = fn,
+        registerPartial: (name, partial) => partials[name] = partial,
+    });
 };
 
 // @description main compiler function
 const mikel = (template = "", data = {}, options = {}) => {
     return create(template, options)(data);
-    // const partials = Object.assign({}, opt.partials || {});
-    // const helpers = Object.assign({}, defaultHelpers, opt.helpers || {});
-    // const variables = Object.assign({}, opt.variables || {}, {root: context});
-    // compile(str.split(tags), output, context, partials, helpers, variables, opt.functions || {}, 0, "");
-    // return output.join("");
 };
 
 // @description assign utilities
+mikel.create = create;
 mikel.escape = escape;
 mikel.get = get;
 mikel.parse = parse;
