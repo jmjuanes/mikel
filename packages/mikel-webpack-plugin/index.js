998148b7961fb7c34503272b20a5a0448d9fc947
Josemi Juanes <5751201+jmjuanes@users.noreply.github.com>
2024-12-07 23:57:29 +0000

    Working on plugin

--- packages/mikel-webpack-plugin/index.js
+++ packages/mikel-webpack-plugin/index.js
@@ -1,3 +1,4 @@
+import path from "node:path";
 // import mikel from "mikel";
 
 // @description global variables
@@ -24,6 +25,38 @@ const generateHtml = (jsFiles, cssFiles) => {
     `;
 };
 
+const getEntryNames = (compilation, includeChunks = []) => {
+    const entryNames = Array.from(compilation.entrypoints.keys());
+    if (!includeChunks || includeChunks.length === 0) {
+        return entryNames;
+    }
+    return entryNames.filter(entryName => !entryName.includes("chunk"));
+};
+
+const getAssets = (compilation, entryNames) => {
+    const publicPath = "./"; 
+    const entryPointPublicPathMap = new Set();
+    const assets = {
+        js: [],
+        css: [],
+    };
+    entryNames.forEach(entryName => {
+        compilation.entrypoints.get(entryName)
+            .getFiles()
+            .filter(file => !!compilation.getAsset(file))
+            .filter(file => [".js", ".css"].includes(path.extname(file)))
+            .map(file => publicPath + file)
+            .forEach(file => {
+                if (!entryPointPublicPathMap.has(file)) {
+                    const ext = path.extname(file).slice(1); // get extension without leading dot
+                    assets[ext].push(file);
+                    entryPointPublicPathMap.add(file);
+                }
+            });
+    });
+    return assets;
+};
+
 // @description MikelWebpackPlugin class
 export default class MikelWebpackPlugin {
     constructor(options = {}) {
@@ -31,18 +64,28 @@ export default class MikelWebpackPlugin {
     }
 
     apply(compiler) {
+        const includeChunks = this.options.chunks || ["main"];
+        const filename = this.options.filename || "index.html";
         compiler.hooks.compilation.tap(PLUGIN_NAME, compilation => {
             const processAssetsOptions = {
                 name: PLUGIN_NAME,
                 stage: compiler.webpack.Compilation.PROCESS_ASSETS_STAGE_OPTIMIZE_INLINE,
             };
-            compilation.hooks.processAssets.tap(processAssetsOptions, assets => {
-                    const jsFiles = Object.keys(assets).filter(file => file.endsWith('.js'));
-                    const cssFiles = Object.keys(assets).filter(file => file.endsWith('.css'));
+            compilation.hooks.processAssets.tap(processAssetsOptions, () => {
+                    // const jsFiles = Object.keys(assets).filter(file => file.endsWith('.js'));
+                    // const cssFiles = Object.keys(assets).filter(file => file.endsWith('.css'));
+                    // console.log(assets);
+                    // const assetInfo = compilation.assetsInfo.get(jsFiles[0]);
+                    // console.log(assetInfo);
+                    // const entryNames = Array.from(compilation.entrypoints.keys());
+                    const entryNames = getEntryNames(compilation, includeChunks);
+                    console.log(entryNames);
+                    const assets = getAssets(compilation, entryNames);
+                    console.log(assets);
 
-                    const htmlContent = generateHtml(jsFiles, cssFiles);
+                    const htmlContent = generateHtml(assets.js, assets.css);
 
-                    compilation.emitAsset('index.html', new compiler.webpack.sources.RawSource(htmlContent));
+                    compilation.emitAsset(filename, new compiler.webpack.sources.RawSource(htmlContent));
                 }
             );
         });
