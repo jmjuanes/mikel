21df1c30f00ca9b8fcaa1b45835dba5a95613b19
Josemi <josemijuanes@gmail.com>
2025-01-15 19:49:26 +0100

    Add types declaration for mikel

---
+++ index.d.ts
@@ -0,0 +1,48 @@
+declare type HelperFunction = (params: {
+    args: any[];
+    opt: Record<string, any>;
+    context: Record<string, any>;
+    fn: (context?: Record<string, any>, vars?: Record<string, any>, output?: string[]) => string;
+}) => string;
+
+declare interface Helpers {
+    [key: string]: HelperFunction;
+}
+
+declare interface Partials {
+    [key: string]: string;
+}
+
+declare interface Functions {
+    [key: string]: (params: {
+        args: any[];
+        opt: Record<string, any>;
+        context: Record<string, any>;
+    }) => string | void;
+}
+
+declare interface MikelOptions {
+    helpers?: Helpers;
+    partials?: Partials;
+    functions?: Functions;
+}
+
+declare interface MikelTemplate {
+    (data?: any): string;
+    addHelper(name: string, fn: HelperFunction): void;
+    removeHelper(name: string): void;
+    addFunction(name: string, fn: (params: any) => string | void): void;
+    removeFunction(name: string): void;
+    addPartial(name: string, partial: string): void;
+    removePartial(name: string): void;
+}
+
+declare const mikel: {
+    (template: string, data?: any, options?: Partial<MikelOptions>): string;
+    create(template: string, options?: Partial<MikelOptions>): MikelTemplate;
+    escape(str: string): string;
+    get(context: any, path: string): any;
+    parse(value: string, context?: any, vars?: any): any;
+};
+
+export default mikel;
