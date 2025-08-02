dbdbd7b8ec24248088c4f307537b140585580dad
Josemi Juanes <5751201+jmjuanes@users.noreply.github.com>
2025-08-02 17:35:50 +0200

    fix: adapt type declarations and fix a typo (#82)

--- index.d.ts
+++ index.d.ts
@@ -1,57 +1,57 @@
-declare type HelperFunction = (params: {
+export type MikelHelper = (params: {
     args: any[];
     opt: Record<string, any>;
-    context: Record<string, any>;
+    tokens: string[];
     data: Record<string, any>;
-    fn: (context?: Record<string, any>, vars?: Record<string, any>, output?: string[]) => string;
+    variables: Record<string, any>;
+    fn: (blockData?: Record<string, any>, blockVars?: Record<string, any>, blockOutput?: string[]) => string;
 }) => string;
 
-declare interface Helpers {
-    [key: string]: HelperFunction;
-}
+export type MikelPartial = {
+    body: string;
+    data: Record<string, any>;
+};
 
-declare interface Partials {
-    [key: string]: string;
-}
+export type MikelFunction = (params: {
+    args: any[];
+    opt: Record<string, any>;
+    data: Record<string, any>;
+    variables: Record<string, any>;
+}) => string | void;
 
-declare interface Functions {
-    [key: string]: (params: {
-        args: any[];
-        opt: Record<string, any>;
-        context: Record<string, any>;
-    }) => string | void;
-}
+export type MikelContext = {
+    helpers: Record<string, MikelHelper>;
+    partials: Record<string, string | MikelPartial>;
+    functions: Record<string, MikelFunction>;
+    variables: Record<string, any>;
+};
 
-declare interface Variables {
-    [key: string]: any;
-}
+export type MikelOptions = {
+    helpers?: Record<string, MikelHelper>;
+    partials?: Record<string, string | MikelPartial>;
+    functions?: Record<string, MikelFunction>;
+    variables?: Record<string, any>;
+};
 
-declare interface MikelOptions {
-    helpers?: Helpers;
-    partials?: Partials;
-    functions?: Functions;
-    variables?: Variables;
-}
-
-declare interface MikelInstance {
+export type Mikel = {
     (template: string, data?: any): string;
-    use(options: Partial<MikelOptions>): MikelTemplate;
-    addHelper(name: string, fn: HelperFunction): void;
+    use(options: Partial<MikelOptions> | ((ctx: MikelContext) => void)): Mikel;
+    addHelper(name: string, fn: MikelHelper): void;
     removeHelper(name: string): void;
-    addFunction(name: string, fn: (params: any) => string | void): void;
+    addFunction(name: string, fn: MikelFunction): void;
     removeFunction(name: string): void;
-    addPartial(name: string, partial: string): void;
+    addPartial(name: string, partial: string | MikelPartial): void;
     removePartial(name: string): void;
 }
 
 declare const mikel: {
     (template: string, data?: any, options?: Partial<MikelOptions>): string;
-    create(options?: Partial<MikelOptions>): MikelInstance;
+    create(options?: Partial<MikelOptions>): Mikel;
     escape(str: string): string;
     get(context: any, path: string): any;
     parse(value: string, context?: any, vars?: any): any;
-    tokenize(value: string): string[];
-    untokenize(value: string[]): string;
+    tokenize(str: string): string[];
+    untokenize(tokens: string[], start?: string, end?: string): string;
 };
 
 export default mikel;
