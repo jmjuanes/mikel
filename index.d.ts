9b3c8837322a1c890241f6942d713e197cccb1d9
Josemi <josemijuanes@gmail.com>
2025-08-02 17:35:24 +0200

    Update and fix some typos

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,10 @@
 export type MikelHelper = (params: {
     args: any[];
     opt: Record<string, any>;
-    context: Record<string, any>;
+    tokens: string[];
     data: Record<string, any>;
-    fn: (context?: Record<string, any>, vars?: Record<string, any>, output?: string[]) => string;
+    variables: Record<string, any>;
+    fn: (blockData?: Record<string, any>, blockVars?: Record<string, any>, blockOutput?: string[]) => string;
 }) => string;
 
 export type MikelPartial = {
@@ -14,24 +15,32 @@ export type MikelPartial = {
 export type MikelFunction = (params: {
     args: any[];
     opt: Record<string, any>;
-    context: Record<string, any>;
+    data: Record<string, any>;
+    variables: Record<string, any>;
 }) => string | void;
+
+export type MikelContext = {
+    helpers: Record<string, MikelHelper>;
+    partials: Record<string, string | MikelPartial>;
+    functions: Record<string, MikelFunction>;
+    variables: Record<string, any>;
+};
 
 export type MikelOptions = {
     helpers?: Record<string, MikelHelper>;
     partials?: Record<string, string | MikelPartial>;
     functions?: Record<string, MikelFunction>;
-    variables?: Variables;
-}
+    variables?: Record<string, any>;
+};
 
 export type Mikel = {
     (template: string, data?: any): string;
-    use(options: Partial<MikelOptions>): Mikel;
+    use(options: Partial<MikelOptions> | ((ctx: MikelContext) => void)): Mikel;
     addHelper(name: string, fn: MikelHelper): void;
     removeHelper(name: string): void;
     addFunction(name: string, fn: MikelFunction): void;
     removeFunction(name: string): void;
-    addPartial(name: string, partial: MikelPartial): void;
+    addPartial(name: string, partial: string | MikelPartial): void;
     removePartial(name: string): void;
 }
 
@@ -41,8 +50,8 @@ declare const mikel: {
     escape(str: string): string;
     get(context: any, path: string): any;
     parse(value: string, context?: any, vars?: any): any;
-    tokenize(value: string): string[];
-    untokenize(value: string[]): string;
+    tokenize(str: string): string[];
+    untokenize(tokens: string[], start?: string, end?: string): string;
 };
 
 export default mikel;
