fd759755b3500ed5cf868044c7c16fd794dda3c2
Josemi Juanes <5751201+jmjuanes@users.noreply.github.com>
2026-02-16 20:52:55 +0100

    Remove code for inline partials

--- index.js
+++ index.js
@@ -220,17 +220,6 @@ const create = (options = {}) => {
                     i = compile(tokens, includeOutput ? output : [], data, state, i + 1, t);
                 }
             }
-            // DEPRECATED
-            else if (tokens[i].startsWith(">*")) {
-                const t = tokens[i].slice(2).trim(), partialTokens = tokens.slice(i + 1);
-                const lastIndex = partialTokens.findIndex((token, j) => {
-                    return j % 2 !== 0 && token.trim().startsWith("/") && token.trim().endsWith(t);
-                });
-                if (typeof ctx.partials[t] === "undefined") {
-                    ctx.partials[t] = untokenize(partialTokens.slice(0, lastIndex));
-                }
-                i = i + lastIndex + 1;
-            }
             else if (tokens[i].startsWith(">")) {
                 const [t, args, opt] = parseArgs(tokens[i].replace(/^>{1,2}/, ""), data, state);
                 const blockContent = []; // to store partial block content
