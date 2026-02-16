5e8869977b301a89a71da6f8ee597603000103bf
Josemi Juanes <5751201+jmjuanes@users.noreply.github.com>
2026-02-16 19:59:48 +0100

    Rename globals -> state

--- index.js
+++ index.js
@@ -64,31 +64,31 @@ const tokenizeArgs = (str = "", tokens = [], strings = []) => {
 };
 
 // @description parse string arguments
-const parseArgs = (str = "", data = {}, globals = {}, fns = {}, argv = [], opt = {}) => {
+const parseArgs = (str = "", data = {}, state = {}, fns = {}, argv = [], opt = {}) => {
     const [t, ...args] = tokenizeArgs(str.trim());
     args.forEach(argStr => {
         if (argStr.includes("=") && !argStr.startsWith(`"`)) {
             const [k, v] = argStr.split("=");
-            opt[k] = parse(v, data, globals, fns);
+            opt[k] = parse(v, data, state, fns);
         }
         else if (argStr.startsWith("...")) {
-            const value = parse(argStr.replace(/^\.{3}/, ""), data, globals, fns);
+            const value = parse(argStr.replace(/^\.{3}/, ""), data, state, fns);
             if (!!value && typeof value === "object") {
                 Array.isArray(value) ? argv.push(...value) : Object.assign(opt, value);
             }
         }
         else {
-            argv.push(parse(argStr, data, globals, fns));
+            argv.push(parse(argStr, data, state, fns));
         }
     });
     return [t, argv, opt];
 };
 
 // @description evaluate an expression
-const evaluateExpression = (str = "", data = {}, globals = {}, fns = {}) => {
-    const [ fnName, args, opt ] = parseArgs(str, data, globals, fns);
+const evaluateExpression = (str = "", data = {}, state = {}, fns = {}) => {
+    const [fnName, args, options] = parseArgs(str, data, state, fns);
     if (typeof fns[fnName] === "function") {
-        return fns[fnName]({args, opt, options: opt, data, variables: globals});
+        return fns[fnName]({ args, options, data, state });
     }
     // if no function has been found with this name
     // throw new Error(`Unknown function '${fnName}'`);
@@ -96,14 +96,14 @@ const evaluateExpression = (str = "", data = {}, globals = {}, fns = {}) => {
 };
 
 // @description parse a string value to a native type
-const parse = (v, data = {}, globals = {}, fns = {}) => {
+const parse = (v, data = {}, state = {}, fns = {}) => {
     if (v.startsWith("(") && v.endsWith(")")) {
-        return evaluateExpression(v.slice(1, -1).trim(), data, globals, fns);
+        return evaluateExpression(v.slice(1, -1).trim(), data, state, fns);
     }
     if ((v.startsWith(`"`) && v.endsWith(`"`)) || /^-?\d+\.?\d*$/.test(v) || v === "true" || v === "false" || v === "null") {
         return JSON.parse(v);
     }
-    return (v || "").startsWith("@") ? get(globals, v.slice(1)) : get(data, v || ".");
+    return (v || "").startsWith("@") ? get(state, v.slice(1)) : get(data, v || ".");
 };
 
 // @description find the index of the closing token
@@ -147,17 +147,17 @@ const defaultHelpers = {
     "escape": p => escape(p.fn(p.data)),
     "raw": p => untokenize(p.tokens),
     "slot": params => {
-        if (typeof params.variables.slot === "undefined") {
-            params.variables.slot = {};
+        if (typeof params.state.slot === "undefined") {
+            params.state.slot = {};
         }
-        params.variables.slot[params.args[0].trim()] = params.fn(params.data);
+        params.state.slot[params.args[0].trim()] = params.fn(params.data);
         return "";
     },
     "macro": params => {
-        if (typeof params.variables.partials === "undefined") {
-            params.variables.partials = {};
+        if (typeof params.state.partials === "undefined") {
+            params.state.partials = {};
         }
-        params.variables.partials[params.args[0].trim()] = untokenize(params.tokens);
+        params.state.partials[params.args[0].trim()] = untokenize(params.tokens);
         return "";
     },
 };
@@ -168,30 +168,29 @@ const create = (options = {}) => {
         helpers: Object.assign({}, defaultHelpers, options?.helpers || {}),
         partials: Object.assign({}, options?.partials || {}),
         functions: options?.functions || {},
-        variables: {},
+        state: {},
     });
     // internal method to compile the template
-    const compile = (tokens, output, data, globals, index = 0, section = "") => {
+    const compile = (tokens, output, data, state, index = 0, section = "") => {
         let i = index;
         while (i < tokens.length) {
             if (i % 2 === 0) {
                 output.push(tokens[i]);
             }
             else if (tokens[i].startsWith("#") && typeof ctx.helpers[tokens[i].slice(1).trim().split(" ")[0]] === "function") {
-                const [t, args, opt] = parseArgs(tokens[i].slice(1), data, globals);
+                const [t, args, opt] = parseArgs(tokens[i].slice(1), data, state);
                 const j = i + 1;
                 i = findClosingToken(tokens, j, t);
                 output.push(ctx.helpers[t]({
                     args: args,
-                    opt: opt, // deprecated
                     options: opt,
                     tokens: tokens.slice(j, i),
                     data: data,
-                    variables: globals,
-                    fn: (blockData = {}, blockGlobals = {}, blockOutput = []) => {
-                        const helperGlobals = {
-                            ...globals,
-                            ...blockGlobals,
+                    state: state,
+                    fn: (blockData = {}, customBlockState = {}, blockOutput = []) => {
+                        const blockState = {
+                            ...state,
+                            ...customBlockState,
                             helper: {
                                 name: t,
                                 options: opt || {},
@@ -199,9 +198,9 @@ const create = (options = {}) => {
                                 context: blockData,
                             },
                             parent: data,
-                            root: globals.root,
+                            root: state.root,
                         };
-                        compile(tokens, blockOutput, blockData, helperGlobals, j, t);
+                        compile(tokens, blockOutput, blockData, blockState, j, t);
                         return blockOutput.join("");
                     },
                 }));
@@ -213,12 +212,12 @@ const create = (options = {}) => {
                 if (!negate && value && Array.isArray(value)) {
                     const j = i + 1;
                     (value.length > 0 ? value : [""]).forEach(item => {
-                        i = compile(tokens, value.length > 0 ? output : [], item, globals, j, t);
+                        i = compile(tokens, value.length > 0 ? output : [], item, state, j, t);
                     });
                 }
                 else {
                     const includeOutput = (!negate && !!value) || (negate && !!!value);
-                    i = compile(tokens, includeOutput ? output : [], data, globals, i + 1, t);
+                    i = compile(tokens, includeOutput ? output : [], data, state, i + 1, t);
                 }
             }
             // DEPRECATED
@@ -233,16 +232,17 @@ const create = (options = {}) => {
                 i = i + lastIndex + 1;
             }
             else if (tokens[i].startsWith(">")) {
-                const [t, args, opt] = parseArgs(tokens[i].replace(/^>{1,2}/, ""), data, globals);
+                const [t, args, opt] = parseArgs(tokens[i].replace(/^>{1,2}/, ""), data, state);
                 const blockContent = []; // to store partial block content
-                const partials = Object.assign({}, ctx.partials, globals.partials || {});
+                const partials = Object.assign({}, ctx.partials, state.partials || {});
                 if (tokens[i].startsWith(">>")) {
-                    i = compile(tokens, blockContent, data, globals, i + 1, t);
+                    i = compile(tokens, blockContent, data, state, i + 1, t);
                 }
                 if (typeof partials[t] === "string" || typeof partials[t]?.body === "string") {
+                    const partialBody = partials[t]?.body || partials[t];
                     const partialData = args.length > 0 ? args[0] : (Object.keys(opt).length > 0 ? opt : data);
-                    const partialGlobals = {
-                        ...globals,
+                    const partialState = {
+                        ...state,
                         content: blockContent.join(""),
                         partial: {
                             name: t,
@@ -252,11 +252,11 @@ const create = (options = {}) => {
                             context: partialData,
                         },
                     };
-                    compile(tokenize(partials[t]?.body || partials[t]), output, partialData, partialGlobals, 0, "");
+                    compile(tokenize(partialBody), output, partialData, partialState, 0, "");
                 }
             }
             else if (tokens[i].startsWith("=")) {
-                output.push(evaluateExpression(tokens[i].slice(1), data, globals, ctx.functions) ?? "");
+                output.push(evaluateExpression(tokens[i].slice(1), data, state, ctx.functions) ?? "");
             }
             else if (tokens[i].startsWith("/")) {
                 if (tokens[i].slice(1).trim() !== section) {
@@ -268,10 +268,10 @@ const create = (options = {}) => {
                 const t = tokens[i].split("||").map(v => {
                     // check if the returned value should not be escaped
                     if (v.trim().startsWith("!")) {
-                        return parse(v.trim().slice(1).trim(), data, globals);
+                        return parse(v.trim().slice(1).trim(), data, state);
                     }
                     // escape the returned value
-                    return escape(parse(v.trim(), data, globals));
+                    return escape(parse(v.trim(), data, state));
                 });
                 output.push(t.find(v => !!v) ?? "");
             }
@@ -281,7 +281,7 @@ const create = (options = {}) => {
     };
     // entry method to compile the template with the provided data object
     const compileTemplate = (template, data = {}, output = []) => {
-        compile(tokenize(template), output, data, { root: data, ...ctx.variables }, 0, "");
+        compile(tokenize(template), output, data, { root: data, ...ctx.state }, 0, "");
         return output.join("");
     };
     // assign api methods and return method to compile the template
@@ -291,9 +291,13 @@ const create = (options = {}) => {
                 newOptions(ctx);
             }
             else if (!!newOptions && typeof newOptions === "object") {
-                ["helpers", "functions", "partials", "variables"].forEach(field => {
+                ["helpers", "functions", "partials", "state"].forEach(field => {
                     Object.assign(ctx[field], newOptions?.[field] || {});
                 });
+                // backward-compatibility with old variables field
+                if (typeof newOptions.variables === "object") {
+                    Object.assign(ctx.state, newOptions.variables || {});
+                }
             }
             return compileTemplate;
         },
