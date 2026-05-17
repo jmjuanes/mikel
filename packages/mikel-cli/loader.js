349c0d19db8e4f539abf0fa83b38157bdd5cc25d
Josemi <josemijuanes@gmail.com>
2026-05-17 16:29:53 +0000

    Fix loader to resolve imports for mikel-cli

--- packages/mikel-cli/loader.js
+++ packages/mikel-cli/loader.js
@@ -5,6 +5,9 @@ registerHooks({
         if (specifier === "mikel") {
             return nextResolve(new URL("../../index.js", import.meta.url).href);
         }
+        else if (specifier === "mikel-cli") {
+            return nextResolve(new URL("./index.js", import.meta.url).href);
+        }
         return nextResolve(specifier, context);
     },
 });
